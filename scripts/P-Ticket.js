d5ec7fbb6ec31d9f1c7a907f0b237afa31e76419
Tajuddin20424 <mttajuddin01@gmail.com>
2024-12-12 12:55:11 +0600

    added the all element htnl, css, js,

---
+++ scripts/P-Ticket.js
@@ -0,0 +1,9 @@
+// Example interaction (optional)
+document.querySelector('.buy-btn').addEventListener('click', () => {
+    alert('Redirecting to ticket purchase page...');
+  });
+  
+  document.querySelector('.see-offers-btn').addEventListener('click', () => {
+    alert('Displaying all offers!');
+  });
+  
